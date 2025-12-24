bef0a04139872d4fe378c1f185c8c0c578c542f3
TalhaAbbas5 <talhaabbas55@gamil.com>
2025-12-24 22:03:20 +0800

    Mofidied files

--- tests/sanity_test.sh
+++ tests/sanity_test.sh
@@ -2,8 +2,12 @@
 
 echo "Running sanity checks..."
 
-test -f server_health_audit.sh || exit 1
-test -x server_health_audit.sh || exit 1
+ls -l server_health_audit.sh || exit 1
+
+test -x server_health_audit.sh || {
+  echo "Script is not executable"
+  exit 1
+}
 
 ./server_health_audit.sh --help >/dev/null 2>&1 || true
 
